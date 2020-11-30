017f0834a12627df6114a455aefadc02f2337f55
lizethblanco08 <74749749+lizethblanco08@users.noreply.github.com>
2020-11-30 07:10:58 -0500

    Se actualiza script de accesos a la base de datos

--- arango_access.py
+++ arango_access.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 
+!pip3 install pyarango
+
+import json
+import requests
+import sys
+
+from pyArango.connection import Connection
+from pyArango.collection import Collection, Edges, Field
+from pyArango.graph import Graph, EdgeDefinition
+from pyArango.collection import BulkOperation as BulkOperation
 
 
+con= Connection(arangoURL='https://3838e90db39f.arangodb.cloud:8529', username='root', password='j0fiXJqRTKCCb9lYZUV2')
+con.getDatabasesURL()
+#db= con.createDatabase('_system')
+db=con['_system']
+db['customers']
